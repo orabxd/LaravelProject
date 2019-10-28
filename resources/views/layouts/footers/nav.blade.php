379505291fa183cc835283166d24b6b435ef80db
SamirPin <SamirPin@users.noreply.github.com>
2019-10-28 14:18:00 -0600

    Verisón retrasada
    Fallos en la carga de mapas, registros y en cuenta de acceso.

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,26 +1,26 @@
 <div class="row align-items-center justify-content-xl-between">
     <div class="col-xl-6">
         <div class="copyright text-center text-xl-left text-muted">
-            &copy; {{ now()->year }} <a href="https://ulsa.edu.ni" class="font-weight-bold ml-1" target="_blank">ULSA</a> &amp;
-            <a href="https://www.ulsa.edu.ni/index.php/programas-academicos/ingenierias/ingenieria-en-cibernetica-electronica#" class="font-weight-bold ml-1" target="_blank">CIBERNETICOS</a>
+            &copy; {{ now()->year }} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a> &amp;
+            <a href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">Updivision</a>
         </div>
     </div>
     <div class="col-xl-6">
         <ul class="nav nav-footer justify-content-center justify-content-xl-end">
             <li class="nav-item">
-                <a href="https://ulsa.edu.ni" class="nav-link" target="_blank">ULSA</a>
+                <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
             </li>
             <li class="nav-item">
-                <a href="https://facebook.com" class="nav-link" target="_blank">Facebook</a>
+                <a href="https://www.updivision.com" class="nav-link" target="_blank">Updivision</a>
             </li>
             <li class="nav-item">
-                <a href="https://twitter.com" class="nav-link" target="_blank">Twitter</a>
+                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
             </li>
             <li class="nav-item">
-                <a href="http://ulsa.edu.ni" class="nav-link" target="_blank">Blog</a>
+                <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
             </li>
             <li class="nav-item">
-                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">Licencia</a>
+                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
             </li>
         </ul>
     </div>
