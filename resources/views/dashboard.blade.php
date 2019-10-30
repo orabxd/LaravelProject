53de56568204f727cd2bb24298fda18e63584606
orabxd <56854966+orabxd@users.noreply.github.com>
2019-10-30 08:45:10 -0600

    xd

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,7 @@
                     <div class="progress-bar bg-gradient-warning" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%;"></div>
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                 </div>
             </div>
         </div>
@@ -239,6 +240,12 @@
             </div>
         </div>
 =======
+    </td>
+>>>>>>> parent of 29b001f... Dashboard and titles
+=======
+                </div>
+            </div>
+        </div>
     </td>
 >>>>>>> parent of 29b001f... Dashboard and titles
 =======
