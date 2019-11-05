5ed06a0720445a7d9562b2a2f491f60f4c50c71a
Dantdes <Dantdes@users.noreply.github.com>
2019-11-05 16:07:19 -0600

    principal
    alv

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app', ['class' => 'bg-default'])
 
 @section('content')
-    <div class="header bg-gradient-primary py-7 py-lg-8">
+    <div class="header bg-gradient-green py-8 py-lg-9">
         <div class="container">
             <div class="header-body text-center mt-7 mb-7">
                 <div class="row justify-content-center">
                     <div class="col-lg-5 col-md-6">
-                        <h1 class="text-white">{{ __('Welcome to Argon Dashboard FREE Laravel Live Preview.') }}</h1>
+                        <h1 class="text-white">{{ __('Bienvenido al Dashboard de la aplicacion de los ciberneticos Sordos de ULSA.') }}</h1>
                     </div>
                 </div>
             </div>
