c81a81563b00c79f3aa8f821caf29793632e19bb
orabxd <56854966+orabxd@users.noreply.github.com>
2019-11-13 09:19:03 -0600

    Revert "XD"
    This reverts commit 78645e326c2fd9c95eae275736420c20c83787f1, reversing
    changes made to 17cbc0b00b86d3545830d7938b485da05e206fbe.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,6 @@
     <div class="container-fluid mt--7">
 
         <div>
-<<<<<<< HEAD
             <div class="col-xl-20 mb-5 mb-xl-0">
                 <div class="card bg-gradient-gray shadow">
 
@@ -25,32 +24,9 @@
                         </div>
                     </div>
                     <div class="card-body">
-=======
-            <div class="col-xl-8 mb--9 mb-xl-0">
-                <div class="card bg-gradient-green shadow">
-
-                    <div class="card-body">
-                        <!-- Chart -->
-                        <div class="chart">
-                            <!-- Chart wrapper -->
-                            <canvas id="chart-sales" class="chart-canvas"></canvas>
-                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-xl-4">
-                <div class="card shadow">
-                    <div class="card-header bg-transparent">
-                        <div class="row align-items-center">
-                            <div class="col">
-                                <h6 class="text-uppercase text-muted ls-1 mb-1">Performance</h6>
-                                <h2 class="mb-0">Total orders</h2>
-                            </div>
-                        </div>
->>>>>>> 84ea877519c42a0995bf3db2171e798b4ba2012f
-                    </div>
-                    <div class="card-body">
-                        <!-- Chart -->
 
         @include('layouts.footers.auth')
     </div>
